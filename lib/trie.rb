6254708d2b020bd0796b456d86847433ba08ddea
James Moriarty <james@telos.co.nz>
2012-06-17 12:14:34 +1200

    Feature: initial commit.

---
+++ lib/trie.rb
@@ -0,0 +1,45 @@
+class Trie
+
+  def initialize
+    @root = {}
+  end
+
+  def build(string)
+    node = @root
+    string.chars.each do |char|
+      node[char] ||= {}
+      node = node[char]
+    end
+    
+    node[:end] = true
+  end
+
+  def include?(word)
+    node = @root
+    word.chars.each do |char|
+      return false if node.nil?
+      node = node[char]
+    end
+    
+    !!node and node[:end]
+  end
+
+  def to_a(node = @root, prefix = "", array = [])
+    return array if node.nil?
+    node.each do |key, value|
+      key == :end ? array << prefix : to_a(value, prefix + key, array)
+    end
+    
+    array
+  end
+
+  def prefixed(prefix, node = @root)
+    prefix.chars.each do |char|
+      return [] unless node[char]
+      node = node[char]
+    end
+    
+    to_a(node).map { |suffix| prefix + suffix }
+  end
+
+end
--- test/test_trie.rb
+++ test/test_trie.rb
@@ -1,7 +1,29 @@
 require 'helper'
 
 class TestTrie < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  def setup
+    @trie = Trie.new
+    @trie.build("more")
+    @trie.build("morph")
   end
+  
+  def test_initialize
+    assert_not_nil @trie
+    assert_equal Trie, @trie.class
+  end
+  
+  def test_include
+    assert @trie.include?("more")
+    assert @trie.include?("morph")
+    assert !@trie.include?("m")
+    assert !@trie.include?("zoo")
+  end
+  
+  def test_prefixed
+    assert_equal @trie.prefixed("z"), []
+    assert_equal @trie.prefixed("m"), ["more", "morph"]
+    assert_equal @trie.prefixed("morp"), ["morph"]
+  end
+
 end
